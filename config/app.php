ff8c2cee271b3a768000a474ce357853280b7c9e
ing27villadiego <ing27villadiego@gmail.com>
2017-04-29 16:15:43 -0500

    Commit installation laravel-collective
    Se instalo laravel collective 5.3
    Y se agrego protected a cada modelo del module one

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
 
 
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -231,6 +232,9 @@ return [
 
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
